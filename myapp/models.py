aeb9ee667dbda235d44b3c2648848e5abe715175
Siyad Savad <siyadsavad313@gmail.com>
2024-10-12 14:09:35 +0530

    database changed and migration problem solved

--- myapp/models.py
+++ myapp/models.py
@@ -2,10 +2,19 @@ from django.db import models
 
 
 
+class Team(models.Model):
+  team_name = models.CharField(max_length=50)
+
+  def __str__(self):
+    return self.team_name
+
+
+
 class Person(models.Model):
   name = models.CharField(max_length=50)
   age = models.IntegerField()
   location = models.CharField(max_length=50)
+  team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name="members")
 
   def __str__(self):
     return self.name
